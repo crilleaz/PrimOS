682012ef809e6dc35256710dac43f38d12846be6
Crille <crilleab@hotmail.com>
2023-08-03 20:23:10 +0200

    Added home directory, changes to ls.php to reflect

--- libs/ls.php
+++ libs/ls.php
@@ -2,9 +2,13 @@
 $manual = 'List files in libs folder.' . PHP_EOL . 'Example usage: ls';
 $version = '1.0';
 
-$dir = "./";
+$dir = "../home";
+if (isset($_GET['input']) && !empty($_GET['input'])) {
+    $dir = $_GET['input'];
+}
 $fl = scandir($dir);
-foreach (scandir($dir) as $fl)
-echo $fl . PHP_EOL;
+foreach ($fl as $file) {
+    echo $file . PHP_EOL;
+}
 
 ?>
